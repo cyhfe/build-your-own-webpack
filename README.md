3d6b5a7b58172b8b8e3ef848caf5e6da33693dfe
chenyuhao <cccchenyuhao@gmail.com>
2022-02-06 22:14:18 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# build-your-own-webpack
+---
+sidebar_position: 1
+---
+
+# 自己实现一个打包工具
+
+## 准备我们的 example ，作为打包的目标
+
+```
+├── bundle.js
+└── example
+  ├── entry.js
+  ├── message.js
+  └── name.js
+```
+
+```js title="entry.js"
+import message from "./message"
+
+console.log(message)
+```
+
+```js title="message.js"
+import message from "./message"
+
+console.log(message)
+```
+
+```js title="name.js"
+import name from "./name"
+export default `hello ${name}`
+```
+
+## 打包工具实现
+
+```js title="bundle.js"
+
+```
---
+++ bundle.js
@@ -0,0 +1,40 @@
+const fs = require("fs")
+const path = require("path")
+const babylon = require("babylon")
+const traverse = require("babel-traverse").default
+
+let ID = 0
+function createAsset(filename) {
+  const content = fs.readFileSync(filename, "utf-8")
+  const ast = babylon.parse(content, {
+    sourceType: "module",
+  })
+
+  const dependencies = []
+
+  traverse(ast, {
+    ImportDeclaration: ({ node }) => {
+      dependencies.push(node.source.value)
+    },
+  })
+
+  const id = ID++
+
+  return {
+    id,
+    filename,
+    dependencies,
+  }
+}
+
+function createGraph(entry) {
+  const mainAsset = createAsset(entry)
+
+  const queue = [mainAsset]
+
+  for (const asset of queue) {
+  }
+}
+
+const graph = createGraph("./example/entry.js")
+console.log(graph)
